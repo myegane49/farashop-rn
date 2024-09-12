21c913de548c3ed79e8e3cb92a8bf64674c847cd
Mohammad A.Yeganeh <m.yegane49@protonmail.com>
2024-09-12 17:05:36 +0330

    Fixed the error with kotlin

--- android/app/src/main/java/com/farashop/MainActivity.kt
+++ android/app/src/main/java/com/farashop/MainActivity.kt
@@ -1,10 +1,10 @@
-import android.os.Bundle;
 package com.farashop
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
 
